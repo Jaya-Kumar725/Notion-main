623d35bcbca1c296e44163c30d5a785d4728ff6f
Jaya kumar N <67946925+Jaya-Kumar725@users.noreply.github.com>
2021-08-23 18:51:52 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import IntroBody from './components/IntroBody';
-import Intro from './components/Intro';
-import Notion from './components/Notion';
-import IntroFooter from './components/IntroFooter';
-import Notionimg from './components/Notionimg';
-import Grid from './components/Grid'
+// import IntroBody from './components/IntroBody';
+// import Intro from './components/Intro';
+// import Notion from './components/Notion';
+// import IntroFooter from './components/IntroFooter';
+// import Notionimg from './components/Notionimg';
+// import Grid from './components/Grid'
 import App from './App';
 
 
